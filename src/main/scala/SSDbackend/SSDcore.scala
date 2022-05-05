1a7f6aac2cd4b6bda3812d9e24f2842ac707f9ca
jiayuah <3414027036@qq.com>
2022-05-05 11:13:30 +0800

    SSDcore: feat     add some new settings

--- src/main/scala/SSDbackend/SSDcore.scala
+++ src/main/scala/SSDbackend/SSDcore.scala
@@ -4,5 +4,10 @@ import chisel3._
 
 case class SSDCoreConfig(
                         EnableDebug: Boolean = true,
-                        EnablePerfCnt: Boolean = true
+                        EnablePipestageDebug : Boolean = false,
+                        EnableLSUDebug : Boolean = false,
+                        EnableStallCnt: Boolean = true,
+                        EnablePerfCnt: Boolean = true,
+                        EnableInstCnt: Boolean = true,
+                        EnableCacheCnt: Boolean = true
                         )
