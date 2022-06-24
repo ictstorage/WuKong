a26028c6e75878dcf8e7b385f2cf0add5a211613
jiayuah <3414027036@qq.com>
2022-06-24 13:20:53 +0800

    ALU: fix syntax error

--- src/main/scala/nutcore/backend/fu/ALU.scala
+++ src/main/scala/nutcore/backend/fu/ALU.scala
@@ -136,8 +136,7 @@ class ALU(hasBru: Boolean = false) extends NutCoreModule {
   io.redirect.target := Mux(!taken && isBranch || ALUInstBPW & !isJump || notALUInstBPW, Mux(isRVC, io.cfIn.pc + 2.U, io.cfIn.pc + 4.U), target)
   // with branch predictor, this is actually to fix the wrong prediction
   io.redirect.valid := valid && isBru && predictWrong //|| branchPredictMiss //|| branchPredictWrong
-  //the jump instruction also needs to update the ghr when the prediction is wrong to prevent the prediction result
-  // of the instruction on the wrong branch from polluting the GHR
+
   io.redirect.ghrUpdateValid := valid && isBru && predictWrong //|| branchPredictMiss || branchPredictWrong // maybe = io.redirect.valid ?
   val redirectRtype = if (EnableOutOfOrderExec) 1.U else 0.U
   io.redirect.btbIsBranch := DontCare
