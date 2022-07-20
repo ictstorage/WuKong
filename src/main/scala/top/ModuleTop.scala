d9f6a926a45aa809241f4af688e3f3df80128440
jiayuah <3414027036@qq.com>
2022-07-20 23:42:38 +0800

    ModuleTop: Modify testModule

--- src/main/scala/top/ModuleTop.scala
+++ src/main/scala/top/ModuleTop.scala
@@ -14,6 +14,7 @@ import top.TopMain.args
 object moduleTop extends App{
   lazy val config = NutCoreConfig(FPGAPlatform = false)
   (new ChiselStage).execute(args, Seq(
-    ChiselGeneratorAnnotation(() => new NutCore()(config)))
+//    ChiselGeneratorAnnotation(() => new NutCore()(config)))
+    ChiselGeneratorAnnotation(() => new testModule))
   )
 }
