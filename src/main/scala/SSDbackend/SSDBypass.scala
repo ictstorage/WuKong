6ca8008bc2169cfa2f5d7fe82b4a16bddbea1ba1
jiayuah <3414027036@qq.com>
2022-10-13 19:06:27 +0800

    SSDBypass: Forbid issuing csr instruction and branch instruction at the same time

--- src/main/scala/SSDbackend/SSDBypass.scala
+++ src/main/scala/SSDbackend/SSDBypass.scala
@@ -156,7 +156,7 @@ class DecodeIO2BypassPkt extends Module {
   io.issueStall(0) := (io.in(0).bits.ctrl.rfSrc1 === i1decodePkt.rd && i0rs1valid ||
     io.in(0).bits.ctrl.rfSrc2 === i1decodePkt.rd && i0rs2valid) && i1decodePkt.rdvalid && i1decodePkt.alu && io.out1.bits.decodePkt.subalu ||
     (i0decodePkt.load || i0decodePkt.store) &&  (i1decodePkt.load || i1decodePkt.store) || (i0decodePkt.csr && i1decodePkt.csr) ||
-    i1decodePkt.branch && i0decodePkt.branch ||          //the condition when load instruction does not meet the launch request
+    (i1decodePkt.branch || i1decodePkt.csr) && (i0decodePkt.csr || i1decodePkt.branch) ||
     (i0decodePkt.muldiv || i0decodePkt.csr) &&
       (i0Hiti1Rs1 || i0Hiti1Rs2 ||
         (i0rs1hitStage >= 4.U && i0rs1hitStage <= 5.U) && FuType(i0rs1hitStage).subalu ||
