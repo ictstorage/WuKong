9c2b0f4fe2a7606d54a66cfdb6288faee3c1453c
jiayuah <3414027036@qq.com>
2022-10-23 13:30:01 +0800

    NutCore: modify MMIO base and size

--- src/main/scala/nutcore/NutCore.scala
+++ src/main/scala/nutcore/NutCore.scala
@@ -78,7 +78,7 @@ object AddressSpace extends HasNutCoreParameter {
   // (start, size)
   // address out of MMIO will be considered as DRAM
   def mmio = List(
-    (0x30000000L, 0x10000000L),  // internal devices, such as CLINT and PLIC
+    (0x00000000L, 0x40000000L),  // internal devices, such as CLINT and PLIC
     (Settings.getLong("MMIOBase"), Settings.getLong("MMIOSize")) // external devices
   )
 
