139fef811eb2eb07a8dd8d6fe51d94f576e5bf68
bigzyb <77113661+bigzyb@users.noreply.github.com>
2022-10-26 01:56:52 +0800

    16KB SRAM PASS

--- src/main/scala/utils/SRAMTemplate.scala
+++ src/main/scala/utils/SRAMTemplate.scala
@@ -19,6 +19,7 @@ package utils
 import chisel3._
 import chisel3.experimental.ExtModule
 import chisel3.util._
+import top.Settings
 class S011HD1P_X32Y2D128 extends ExtModule with HasExtModuleResource {
   //  val io = IO(new Bundle {
   val Q =   IO(Output(UInt(128.W)))
@@ -57,20 +58,64 @@ class SramIO extends Bundle {
   val en: Bool = Input(Bool())
   val idx: UInt =Input(UInt(6.W))
   val wen: Bool =Input(Bool())
-  val wMask: UInt = Input(UInt(128.W))
+//  val wMask: UInt = Input(UInt(128.W))
+  val wData: UInt = Input(UInt(128.W))
+}
+class SramIO4 extends Bundle {
+  val rData: UInt = Output(UInt(128.W))
+
+  val en: Bool = Input(Bool())
+  val idx: UInt =Input(UInt(9.W))
+  val wen: Bool =Input(Bool())
+//  val wMask: UInt = Input(UInt(128.W))
   val wData: UInt = Input(UInt(128.W))
 }
 
 class Sram extends Module {
   val io = IO(new SramIO)
-  val sram: S011HD1P_X32Y2D128_BW = Module(new S011HD1P_X32Y2D128_BW)
+  val sram: S011HD1P_X32Y2D128 = Module(new S011HD1P_X32Y2D128)
   io.rData  := sram.Q
   sram.CLK  := clock
   sram.CEN  := !io.en
   sram.A    := io.idx
   sram.WEN  := !io.wen
-  sram.BWEN := ~io.wMask
+//  sram.BWEN := ~io.wMask
   sram.D    := io.wData
+}
+class Sram4 extends Module {
+  val io = IO(new SramIO4)
+  val sram = Seq.fill(4)(Module(new S011HD1P_X32Y2D128_BW))
+  for(i <-0 until 4) {
+    when (i.U === io.idx(2, 1)) {
+//      io.rData := sram(i).Q
+      sram(i).CLK := clock
+      sram(i).CEN := !io.en
+      sram(i).A := io.idx(8, 3)
+      sram(i).WEN := !io.wen
+      sram(i).D := Mux(io.idx(0), Cat(io.wData,0.U(64.W)), Cat(0.U(64.W), io.wData))
+      sram(i).BWEN := Mux(io.idx(0), "h0000_0000_0000_0000_ffff_ffff_ffff_ffff".U,
+        "hffff_ffff_ffff_ffff_0000_0000_0000_0000".U)
+    }.elsewhen(i.U =/= io.idx(2, 1)) {
+//      io.rData := sram(i).Q
+      sram(i).CLK := clock
+      sram(i).CEN := true.B
+      sram(i).A := io.idx(8, 3)
+      sram(i).WEN := true.B
+      sram(i).D := 0.U
+      sram(i).BWEN := "hffff_ffff_ffff_ffff_ffff_ffff_ffff_ffff".U
+    }
+  }
+  val rdatasel = WireInit(0.U)
+  when (0.U === RegNext(io.idx(2, 1))) {
+    rdatasel := sram(0).Q
+  }.elsewhen(1.U === RegNext(io.idx(2, 1))) {
+    rdatasel := sram(1).Q
+  }.elsewhen(2.U === RegNext(io.idx(2, 1))) {
+    rdatasel := sram(2).Q
+  }.otherwise {
+    rdatasel := sram(3).Q
+  }
+  io.rData := Mux((RegNext(io.idx(0))), rdatasel(127,64), rdatasel(63,0))
 }
 class SRAMBundleAW[T <: Data](private val gen: T, set: Int, val way: Int = 1) extends SRAMBundleA(set) {
   val data = Output(gen)
@@ -155,17 +200,17 @@ class SRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
   }
 }
 
-class ysyxSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
-                              shouldReset: Boolean = false, holdRead: Boolean = false, singlePort: Boolean = true) extends Module {
+class ysyxDataSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
+                                  shouldReset: Boolean = false, holdRead: Boolean = false, singlePort: Boolean = false) extends Module {
   val io = IO(new Bundle {
     val r = Flipped(new SRAMReadBus(gen, set, way))
     val w = Flipped(new SRAMWriteBus(gen, set, way))
   })
 
+  require(!holdRead)
   val wordType = UInt(gen.getWidth.W)
-  val array = SyncReadMem(set, Vec(way, wordType))
-  val sramnum = 2
-  val sramarray = Seq.fill(sramnum)(Module(new Sram()))
+  // val array = SyncReadMem(set, Vec(way, wordType))
+  val sram = Seq.fill(way)(Module(new S011HD1P_X32Y2D128()))
   val (resetState, resetSet) = (WireInit(false.B), WireInit(0.U))
 
   if (shouldReset) {
@@ -184,23 +229,19 @@ class ysyxSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
   val wdataword = Mux(resetState, 0.U.asTypeOf(wordType), io.w.req.bits.data.asUInt)
   val waymask = Mux(resetState, Fill(way, "b1".U), io.w.req.bits.waymask.getOrElse("b1".U))
   val wdata = VecInit(Seq.fill(way)(wdataword))
-  when (wen) { array.write(setIdx, wdata, waymask.asBools) }
-//  val Sram_read   = (WireInit(VecInit(Seq.fill(way)(Cat(0.U((128/way-gen.getWidth).W), wdataword)))))
-  val Sram_write  = WireInit(VecInit(Seq.fill(way)(Cat(0.U((128/way-gen.getWidth).W), wdataword))))
-  val Sram_read = RegInit(0.U(128.W))
-  for(i<- 0 until sramnum) {
-    sramarray(i).io.en    := ((wen || realRen) && (i.U===io.r.req.bits.setIdx(6)))
-    sramarray(i).io.wen   := (wen && (i.U===io.r.req.bits.setIdx(6)))
-    sramarray(i).io.idx   := Mux(wen, setIdx(5,0), io.r.req.bits.setIdx(5,0))
-    sramarray(i).io.wData := Sram_write.asUInt()
-    sramarray(i).io.wMask := FillInterleaved(128/way, waymask)
-    when((wen || realRen) && (i.U===io.r.req.bits.setIdx(6))) {
-      Sram_read             := sramarray(i).io.rData
-    }
+  // when (wen) { array.write(setIdx, wdata, waymask.asBools) }
+
+  sram.map(_.CLK := clock)
+  sram.map(_.A := Mux(wen, setIdx, io.r.req.bits.setIdx))
+  sram.zipWithIndex.map{
+    case (s, i) => s.CEN := ~(wen || realRen)
   }
-  val rdata = Sram_read.asTypeOf(Vec(way, UInt((128/way).W))).map(_.asTypeOf(gen))
-//  val rdata = (if (holdRead) ReadAndHold(array, io.r.req.bits.setIdx, realRen)
-//  else array.read(io.r.req.bits.setIdx, realRen)).map(_.asTypeOf(gen))
+  sram.zipWithIndex.map{
+    case (s, i) => s.WEN := ~(wen && OHToUInt(io.w.req.bits.waymask.getOrElse("b0".U)) === i.U)
+  }
+  sram.map(_.D := wdataword)
+
+  val rdata = VecInit(sram.map(_.Q)).map(_.asTypeOf(gen))
   io.r.resp.data := VecInit(rdata)
 
   io.r.req.ready := !resetState && (if (singlePort) !wen else true.B)
@@ -214,6 +255,65 @@ class ysyxSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
       printf("%d: SRAMTemplate: read %x at idx = %d\n", GTimer(), VecInit(rdata).asUInt, RegNext(io.r.req.bits.setIdx))
     }
   }
+}
+
+
+class ysyxSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
+                              shouldReset: Boolean = false, holdRead: Boolean = false, singlePort: Boolean = true) extends Module {
+  val io = IO(new Bundle {
+    val r = Flipped(new SRAMReadBus(gen, set, way))
+    val w = Flipped(new SRAMWriteBus(gen, set, way))
+  })
+
+  val wordType = UInt(gen.getWidth.W)
+//  val array = SyncReadMem(set, Vec(way, wordType))
+//  val sramnum = 32
+  val sram = Seq.fill(4)(Module(new Sram4()))
+  val (resetState, resetSet) = (WireInit(false.B), WireInit(0.U))
+
+  if (shouldReset) {
+    val _resetState = RegInit(true.B)
+    val (_resetSet, resetFinish) = Counter(_resetState, set)
+    when (resetFinish) { _resetState := false.B }
+
+    resetState := _resetState
+    resetSet := _resetSet
+  }
+
+  val (ren, wen) = (io.r.req.valid, io.w.req.valid || resetState)
+  val realRen = (if (singlePort) ren && !wen else ren)
+
+  val setIdx = Mux(resetState, resetSet, io.w.req.bits.setIdx)
+  val wdataword = Mux(resetState, 0.U.asTypeOf(wordType), io.w.req.bits.data.asUInt)
+  val waymask = Mux(resetState, Fill(way, "b1".U), io.w.req.bits.waymask.getOrElse("b1".U))
+  val wdata = VecInit(Seq.fill(way)(wdataword))
+//  when (wen) { array.write(setIdx, wdata, waymask.asBools) }
+//  val Sram_read   = (WireInit(VecInit(Seq.fill(way)(Cat(0.U((128/way-gen.getWidth).W), wdataword)))))
+//  val Sram_write  = WireInit(VecInit(Seq.fill(way)(Cat(0.U((128/way-gen.getWidth).W), wdataword))))
+  val Sram_read = WireInit(VecInit(Seq.fill(way)(0.U(64.W))))
+
+  sram.map(_.io.idx := Mux(wen, setIdx, io.r.req.bits.setIdx))
+  sram.zipWithIndex.map{
+    case (s, i) => s.io.en := (wen || realRen)
+  }
+  sram.zipWithIndex.map{
+    case (s, i) => s.io.wen := (wen && OHToUInt(io.w.req.bits.waymask.getOrElse("b0".U)) === i.U)
+  }
+  sram.map(_.io.wData := wdataword)
+  val rdata = VecInit(sram.map(_.io.rData))
+  io.r.resp.data := VecInit(rdata.map(_.asTypeOf(gen)))
+
+  io.r.req.ready := !resetState && (if (singlePort) !wen else true.B)
+  io.w.req.ready := true.B
+
+//  Debug(false) {
+//    when (wen) {
+//      printf("%d: SRAMTemplate: write %x to idx = %d\n", GTimer(), wdata.asUInt, setIdx)
+//    }
+//    when (RegNext(realRen)) {
+//      printf("%d: SRAMTemplate: read %x at idx = %d\n", GTimer(), VecInit(rdata).asUInt, RegNext(io.r.req.bits.setIdx))
+//    }
+//  }
 }
 class SRAMTemplateWithArbiter[T <: Data](nRead: Int, gen: T, set: Int, way: Int = 1,
                                          shouldReset: Boolean = false) extends Module {
@@ -374,7 +474,7 @@ class DataSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
   require(!holdRead)
   val wordType = UInt(gen.getWidth.W)
   // val array = SyncReadMem(set, Vec(way, wordType))
-  val sram = Seq.fill(way)(Module(new S011HD1P_X32Y2D128()))
+  val sram = Seq.fill(way)(Module(new Sram()))
   val (resetState, resetSet) = (WireInit(false.B), WireInit(0.U))
 
   if (shouldReset) {
@@ -395,30 +495,30 @@ class DataSRAMTemplate[T <: Data](gen: T, set: Int, way: Int = 1,
   val wdata = VecInit(Seq.fill(way)(wdataword))
   // when (wen) { array.write(setIdx, wdata, waymask.asBools) }
 
-  sram.map(_.CLK := clock)
-  sram.map(_.A := Mux(wen, setIdx, io.r.req.bits.setIdx))
+//  sram.map(_.CLK := clock)
+  sram.map(_.io.idx := Mux(wen, setIdx(5,0), io.r.req.bits.setIdx(5,0)))
   sram.zipWithIndex.map{
-    case (s, i) => s.CEN := ~(wen || realRen)
+    case (s, i) => s.io.en := (wen || realRen)
   }
   sram.zipWithIndex.map{
-    case (s, i) => s.WEN := ~(wen && OHToUInt(io.w.req.bits.waymask.getOrElse("b0".U)) === i.U)
+    case (s, i) => s.io.wen := (wen && OHToUInt(io.w.req.bits.waymask.getOrElse("b0".U)) === i.U)
   }
-  sram.map(_.D := wdataword)
-
-  val rdata = VecInit(sram.map(_.Q)).map(_.asTypeOf(gen))
-  io.r.resp.data := VecInit(rdata)
+  sram.map(_.io.wData := Mux(io.w.req.bits.setIdx(6), Cat(wdataword,(0.U(64.W))), Cat((0.U(64.W)),wdataword)))
+//  sram.map(_.io.wMask := Mux(io.w.req.bits.setIdx(6),  Cat(VecInit(Seq.fill(64)(true.B)).asUInt(), VecInit(Seq.fill(64)(false.B)).asUInt()), Cat(VecInit(Seq.fill(64)(false.B)).asUInt(), VecInit(Seq.fill(64)(true.B)).asUInt())))
+  val rdata = Mux(io.r.req.bits.setIdx(6),  VecInit(sram.map(_.io.rData(127, 64))),  VecInit(sram.map(_.io.rData(63, 0))))
+  io.r.resp.data := VecInit(rdata.map(_.asTypeOf(gen)))
 
   io.r.req.ready := !resetState && (if (singlePort) !wen else true.B)
   io.w.req.ready := true.B
 
-  Debug(false) {
-    when (wen) {
-      printf("%d: SRAMTemplate: write %x to idx = %d\n", GTimer(), wdata.asUInt, setIdx)
-    }
-    when (RegNext(realRen)) {
-      printf("%d: SRAMTemplate: read %x at idx = %d\n", GTimer(), VecInit(rdata).asUInt, RegNext(io.r.req.bits.setIdx))
-    }
-  }
+//  Debug(false) {
+//    when (wen) {
+//      printf("%d: SRAMTemplate: write %x to idx = %d\n", GTimer(), wdata.asUInt, setIdx)
+//    }
+//    when (RegNext(realRen)) {
+//      printf("%d: SRAMTemplate: read %x at idx = %d\n", GTimer(), VecInit(rdata).asUInt, RegNext(io.r.req.bits.setIdx))
+//    }
+//  }
 }
 
 //class SRAMTemplateWithArbiter[T <: Data](nRead: Int, gen: T, set: Int, way: Int = 1,
